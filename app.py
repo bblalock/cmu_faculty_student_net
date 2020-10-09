35ec8e1bb1fa3979498dd78e7bbd0703df8be908
Benjamin Blalock <benjaminblalock@benjamins-mbp.home>
2020-10-09 12:25:08 -0400

    initial app boilerplate

--- app.py
+++ app.py
@@ -2,11 +2,10 @@ import dash_html_components as html
 from layout import get_layout
 from callbacks import *
 
+server = app.server
 app.title = 'CMU NET'
 app.layout = html.Div(children=get_layout(cyto_elements))
 
-server = app.server
-
 # Main
 if __name__ == "__main__":
-    app.run_server(debug=True)
+    app.run_server(debug=False)
