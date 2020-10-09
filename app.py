f29c24237d59727705136971bbe7735271fd7b8b
Benjamin Blalock <benjaminblalock@benjamins-mbp.home>
2020-10-09 12:11:24 -0400

    initial app boilerplate

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ from callbacks import *
 app.title = 'CMU NET'
 app.layout = html.Div(children=get_layout(cyto_elements))
 
+server = app.server
+
 # Main
 if __name__ == "__main__":
-    app.run_server(debug=False)
+    app.run_server(debug=True)
--- callbacks/controls.py
+++ callbacks/controls.py
@@ -1,6 +1,5 @@
-from dash.dependencies import Input, Output, State
-from app_setup import app, cyto_elements
-from utils.cyto import cyto_network
+from dash.dependencies import Input, Output
+from app_setup import app
 
 @app.callback([Output('edge_weight_slider_comm', 'max'),
                Output('edge_weight_slider_comm', 'marks'),
@@ -32,21 +31,3 @@ def set_max_edge_weight(elements, comm_value, adv_value):
     return [max_weight['co_committee_edge'], marks['co_committee_edge'],
             max_weight['co_advised_edge'], marks['co_advised_edge'],
             ]
-
-# @app.callback(Output('cmu_net_loading', 'children'),
-#               [Input('node_filter_dropdown', 'value')],
-#               [State('cmu_net', 'elements')]
-#               )
-# def filter_nodes(nodes_to_include, elements):
-#     print(nodes_to_include)
-#     if not nodes_to_include:
-#         return [cyto_network(elements, is_dict=False)]
-#     faculty_types = ['core_faculty', 'affiliated_faculty', 'related_faculty', 'unknown_faculty']
-#     new_elements = [ele for ele in elements if ele['classes'] != 'faculty_node' and 'edge' != ele['classes']]
-#     for item in nodes_to_include:
-#         if item == 'faculty':
-#             for ftype in faculty_types:
-#                 new_elements = new_elements + cyto_elements[ftype]
-#         else:
-#             new_elements = new_elements + cyto_elements[item]
-#     return [cyto_network(new_elements, is_dict=False)]
